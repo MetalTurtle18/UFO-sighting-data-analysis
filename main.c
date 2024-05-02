4c808322eb93d6614c8ece2a543936362379ee98
MetalTurtle18 <dekolis1218@gmail.com>
2024-05-02 17:44:52 -0400

    Begin file parsing code

--- main.c
+++ main.c
@@ -1,6 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define COLUMNS 10
+//#define ROWS 80332
+#define ROWS 10 // TODO debug number
+#define MAX_CITY 70 // 69 characters is the longest city name
+#define MAX_SHAPE 10 // 9 characters is the longest shape name
+#define MAX_COMMENT 236 // 235 characters is the longest comment
+
+typedef struct date { // MM/DD/YYYY format in csv
+    int year;
+    int month;
+    int day;
+} date;
+
+typedef struct dateTime {
+    date date;
+    int hour;
+    int minute;
+} dateTime;
+
+typedef struct sightingNode {
+    dateTime dateTime;
+    char city[MAX_CITY];
+    char state[2]; // Note: NOT string
+    char country[2]; // Note: NOT string
+    char shape[MAX_SHAPE];
+    int duration;
+    char comment[MAX_COMMENT];
+    date dateReported;
+    double latitude;
+    double longitude;
+    struct sightingNode *next;
+} sightingNode;
+
+void loadData(char fileName[], sightingNode *head);
+
+void readDate(FILE *csv, date *date);
+
+void readTime(FILE *csv, dateTime *dateTime);
+
+void readSpacedString(FILE *csv, char string[]);
+
+void freeData(sightingNode *head);
 
 int main(void) {
-    printf("Hello, World!\n");
+    sightingNode *headNode = malloc(sizeof(sightingNode));
+
+    loadData("../scrubbed.csv", headNode);
+
+    fflush(stdout);
+
+//    freeData(headNode);
+    free(headNode);
     return 0;
+}
+
+void loadData(char fileName[], sightingNode *head) {
+    FILE *csv = fopen(fileName, "r");
+    int line;
+    char c;
+
+    char date[11];
+    char time[6];
+    char city[MAX_CITY];
+
+    for (line = 0; line < ROWS; line++) {
+        fscanf(csv, "%s", date);
+        fseek(csv, 1, SEEK_CUR);
+        fscanf(csv, "%5s", time);
+        fseek(csv, 1, SEEK_CUR);
+        readSpacedString(csv, city);
+        // TODO: more reading
+    }
+
+    fclose(csv);
+}
+
+void readDate(FILE *csv, date *date) {
+
+}
+
+void readTime(FILE *csv, dateTime *dateTime) {
+
+}
+
+void readSpacedString(FILE *csv, char string[]) {
+    char c = ' ';
+    int i = 0;
+    while (c != ',') {
+        fscanf(csv, "%c", &c);
+        if (c == ',')
+            string[i] = '\0';
+        else
+            string[i] = c;
+        i++;
+    }
 }
