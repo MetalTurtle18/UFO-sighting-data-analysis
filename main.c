0674e5f2bc3588461ec0abb01201beb1afeea530
MetalTurtle18 <dekolis1218@gmail.com>
2024-05-05 00:02:33 -0400

    Create menu function

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define MAX_CITY 70 // 69 characters is the longest city name
 #define MAX_SHAPE 10 // 9 characters is the longest shape name
 #define MAX_COMMENT 236 // 235 characters is the longest comment
-#define MAX_DURATION_CHARS 10
+#define MAX_MENU_OPTION 50
 
 typedef struct date { // MM/DD/YYYY format in csv
     int year;
@@ -24,8 +24,8 @@ typedef struct dateTime {
 typedef struct sightingNode {
     dateTime dateTime;
     char city[MAX_CITY];
-    char state[2]; // Note: NOT string
-    char country[2]; // Note: NOT string
+    char state[3];
+    char country[3];
     char shape[MAX_SHAPE];
     int duration;
     char comment[MAX_COMMENT];
@@ -37,71 +37,60 @@ typedef struct sightingNode {
 
 void loadData(char fileName[], sightingNode *head);
 
-void readDate(FILE *csv, date *date);
-
-void readTime(FILE *csv, dateTime *dateTime);
-
 void readStringToSpacer(FILE *csv, char string[]);
 
 void freeData(sightingNode *head);
+
+int in(char c, char arr[], int len);
+
+char menu(char message[], char optionsText[][MAX_MENU_OPTION], char options[], int numOptions, int defaultOption);
 
 int main(void) {
     sightingNode *headNode = malloc(sizeof(sightingNode));
 
     loadData("../scrubbed.csv", headNode);
 
-    fflush(stdout);
+    // TODO TESTING
+    char optionsText[][MAX_MENU_OPTION] = {"Number 1", "number 2", "opt 3"};
+    char options[] = {'a', 'b', 'c'};
+    char l = menu("here is menu", optionsText, options, 3, 0);
+    printf("a%ca", l);
 
-//    freeData(headNode);
-    free(headNode);
+    freeData(headNode);
     return 0;
 }
 
 void loadData(char fileName[], sightingNode *head) {
     FILE *csv = fopen(fileName, "r");
     int line;
-    char c;
-
-    char date[11];
-    char time[6];
-    char city[MAX_CITY];
-    char duration[MAX_DURATION_CHARS];
+    sightingNode *node = head;
 
     for (line = 0; line < ROWS; line++) {
-        fscanf(csv, "%s", date);
-        fseek(csv, 1, SEEK_CUR);
-        fscanf(csv, "%5s", time);
-        fseek(csv, 1, SEEK_CUR);
-        readStringToSpacer(csv, city);
-        fseek(csv, 1, SEEK_CUR);
-        fscanf(csv, "%c", &c);
-        if (c != ',') {
-            head->state[0] = c;
-            fscanf(csv, "%c", &head->state[1]);
-            fseek(csv, 1, SEEK_CUR);
-        }
-        fscanf(csv, "%c", &c);
-        if (c != ',') {
-            head->country[0] = c;
-            fscanf(csv, "%c", &head->country[1]);
-            fseek(csv, 1, SEEK_CUR);
-        }
-        readStringToSpacer(csv, head->shape);
-        fseek(csv, 1, SEEK_CUR);
-        readStringToSpacer(csv, duration);
-        fseek(csv, 1, SEEK_CUR);
-        // TODO: more reading
+        fscanf(csv, "%d/%d/%4d %2d:%2d,",
+               &node->dateTime.date.month,
+               &node->dateTime.date.day,
+               &node->dateTime.date.year,
+               &node->dateTime.hour,
+               &node->dateTime.minute
+        );
+        readStringToSpacer(csv, node->city);
+        readStringToSpacer(csv, node->state);
+        readStringToSpacer(csv, node->country);
+        readStringToSpacer(csv, node->shape);
+        fscanf(csv, "%d,", &node->duration);
+        readStringToSpacer(csv, node->comment);
+        fscanf(csv, "%d/%d/%4d,%lf,%lf\n",
+               &node->dateReported.month,
+               &node->dateReported.day,
+               &node->dateReported.year,
+               &node->longitude,
+               &node->latitude
+        );
+        node = malloc(sizeof(sightingNode));
+        head->next = node;
     }
 
     fclose(csv);
-}
-
-void readDate(FILE *csv, date *date) {
-
-}
-
-void readTime(FILE *csv, dateTime *dateTime) {
-
 }
 
 void readStringToSpacer(FILE *csv, char string[]) {
@@ -115,5 +104,44 @@ void readStringToSpacer(FILE *csv, char string[]) {
             string[i] = c;
         i++;
     }
-    fseek(csv, -1, SEEK_CUR);
+}
+
+void freeData(sightingNode *head) {
+    if (head->next == NULL)
+        free(head);
+    else
+        freeData(head->next);
+}
+
+int in(char c, char arr[], int len) {
+    int i;
+    for (i = 0; i < len; i++)
+        if (arr[i] == c)
+            return 1;
+    return 0;
+}
+
+char menu(char message[], char optionsText[][MAX_MENU_OPTION], char options[], int numOptions, int defaultOption) {
+    int i;
+    int first = 1;
+    char out, _;
+    printf("%s\n", message);
+    for (i = 0; i < numOptions; i++)
+        printf("%s (%c)\n", optionsText[i], options[i]);
+    printf("> ");
+
+    do {
+        if (!first) {
+            printf("Invalid option. Try again\n> ");
+            scanf("%c", &_);
+        }
+        scanf("%c", &out);
+        first = 0;
+    } while (!in(out, options, numOptions) && out != '\n');
+
+    if (out == '\n')
+        return options[defaultOption];
+    else
+        scanf("%c", &_);
+    return out;
 }
